e5727a5efe0b4ca136eda2e10514bf1592352066
Vlad <vladislavmoiseenkov@gmail.com>
2019-05-25 21:06:14 +0300

    init

---
+++ app.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const morgan = require('morgan');
+const { sequelize } = require('./models');
+const config = require('./config');
+
+const app = express();
+app.use(morgan('combined'));
+app.use(bodyParser.json());
+app.use(cors());
+
+require('./routes')(app);
+
+app.listen(config.port);
